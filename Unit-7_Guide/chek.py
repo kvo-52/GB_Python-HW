933d90f67ac9243ab25a680413da465a6552d200
kvo.52@mail.ru <kvo.52@mail.ru>
2023-01-30 11:05:35 +0300

    delet folder

--- Unit-7_Guide/chek.py
+++
@@ -1,7 +0,0 @@
-def digit_check():
-    try:
-        input_number = int(input('Введите число: \n'))
-        return input_number
-    except ValueError:
-        print('Это должно быть число\n')
-        return digit_check()
--- Unit-7_Guide/main.py
+++
@@ -1,5 +0,0 @@
-import controller as c
-import user_interface as ui
-
-ui.start()
-c.user_choice ()
